ba050f9ceed6459a11d5d623499985d161a3bf7e
Valeria Arnao <varnao@calpoly.edu>
2025-04-14 10:42:50 -0700

    steps 2-5 of lab 2

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -1,17 +1,66 @@
 // index.js
 import express from "express";
 
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
+
